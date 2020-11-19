4a94fddc7f390f79a1182e6fc6f292945131cc05
Ezra Gunn <ezracodes@gmail.com>
2020-11-19 10:44:45 +0800

    Added theme and colours

---
+++ lib/resources/colours.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const Color setelBlue50 = Color(0xFFE0F6FF);
+const Color setelBlue100 = Color(0xFFB3E7FF);
+const Color setelBlue200 = Color(0xFF80D8FF);
+const Color setelBlue300 = Color(0xFF4DC8FF);
+const Color setelBlue400 = Color(0xFF26BCFF);
+const Color setelBlue500 = Color(0xFF00B0FF);
+const Color setelBlue600 = Color(0xFF00A9FF);
+const Color setelBlue700 = Color(0xFF00A0FF);
+const Color setelBlue800 = Color(0xFF0097FF);
+const Color setelBlue900 = Color(0xFF0087FF);
+
+const Color setelBlueAccent100 = Color(0xFFFFFFFF);
+const Color setelBlueAccent200 = Color(0xFFF2F8FF);
+const Color setelBlueAccent400 = Color(0xFFBFDDFF);
+const Color setelBlueAccent700 = Color(0xFFA6D0FF);
---
+++ lib/resources/theme.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:setel_geofence/resources/colours.dart';
+
+final ThemeData _theme = _buildTheme();
+
+ThemeData _buildTheme() {
+  final ThemeData base = ThemeData.light();
+
+  return base.copyWith(
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    primaryColor: setelBlue500,
+    accentColor: Colors.white,
+    primaryColorBrightness: Brightness.dark,
+    accentColorBrightness: Brightness.dark,
+    appBarTheme: AppBarTheme(
+      color: Colors.white,
+      textTheme: _buildTextTheme(base.textTheme),
+      centerTitle: false,
+      brightness: Brightness.light,
+    ),
+    textSelectionColor: setelBlue500.withOpacity(0.2),
+    textSelectionHandleColor: setelBlue500,
+    scaffoldBackgroundColor: Colors.white,
+    canvasColor: Colors.white,
+    dialogTheme: DialogTheme(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    ),
+    bottomSheetTheme: const BottomSheetThemeData(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16.0),
+          topRight: Radius.circular(16.0),
+        ),
+      ),
+    ),
+    snackBarTheme: const SnackBarThemeData(
+      behavior: SnackBarBehavior.floating,
+    ),
+    cardTheme: CardTheme(
+        shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(16.0),
+    )),
+    tabBarTheme: TabBarTheme(
+      unselectedLabelColor: setelBlue500.withOpacity(0.5),
+      labelColor: setelBlue500,
+    ),
+    buttonTheme: ButtonThemeData(
+      shape: const StadiumBorder(),
+      buttonColor: setelBlue500,
+      disabledColor: Colors.black.withOpacity(0.6),
+      textTheme: ButtonTextTheme.primary,
+      colorScheme: base.colorScheme.copyWith(
+        primary: setelBlue500,
+        secondary: setelBlue500,
+      ),
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: setelBlue500,
+      disabledElevation: 0.0,
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      border: OutlineInputBorder(),
+    ),
+    textTheme: _buildTextTheme(base.textTheme),
+    primaryTextTheme: _buildTextTheme(base.primaryTextTheme),
+    accentTextTheme: _buildTextTheme(base.accentTextTheme),
+    primaryIconTheme: base.primaryIconTheme.copyWith(color: Colors.black),
+  );
+}
+
+TextTheme _buildTextTheme(TextTheme base) {
+  return base.copyWith(
+    headline6: base.headline6.copyWith(
+      fontFamily: GoogleFonts.cabin().fontFamily,
+      fontSize: 24.0,
+    ),
+  );
+}
+
+ThemeData getThemeData(BuildContext context) {
+  return _theme;
+}
