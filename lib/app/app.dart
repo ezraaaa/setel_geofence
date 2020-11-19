7dfe488fedca329b3255454ce70d3652ff95e03e
Ezra Gunn <ezracodes@gmail.com>
2020-11-19 10:45:13 +0800

    Added orientation and system ui style

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:setel_geofence/resources/theme.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+        systemNavigationBarDividerColor: Colors.transparent,
+      ),
+      child: MaterialApp(
+        title: 'Setel',
+        theme: getThemeData(context),
+      ),
+    );
   }
 }
