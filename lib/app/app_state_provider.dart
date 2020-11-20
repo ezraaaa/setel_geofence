bcd34397d34a7432a6a19e5856c9719968fd6539
Ezra Gunn <ezracodes@gmail.com>
2020-11-20 09:17:53 +0800

    added repository providers and bloc providers

--- lib/app/app_state_provider.dart
+++ lib/app/app_state_provider.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:setel_geofence/app/app.dart';
+import 'package:setel_geofence/home/blocs/bloc/permission_bloc.dart';
+import 'package:setel_geofence/repositories/permission/permission_repository.dart';
 
 class AppStateProvider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return App();
+    return MultiRepositoryProvider(
+      providers: <RepositoryProvider<dynamic>>[
+        RepositoryProvider<PermissionRepository>(
+          create: (_) => PermissionRepository(),
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: <BlocProvider<dynamic>>[
+          BlocProvider<PermissionBloc>(
+            create: (BuildContext context) {
+              return PermissionBloc(
+                permissionRepository:
+                    RepositoryProvider.of<PermissionRepository>(context),
+              )..add(RequestLocationPermission());
+            },
+          ),
+        ],
+        child: App(),
+      ),
+    );
   }
 }
