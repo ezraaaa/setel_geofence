d2f903e8a7506e3b18828d5c2301f7da16086842
Ezra Gunn <ezracodes@gmail.com>
2020-11-24 00:25:47 +0800

    Change app bar background colour to transparent

--- lib/home/views/home_page.dart
+++ lib/home/views/home_page.dart
@@ -23,6 +23,7 @@ class _HomePageState extends State<HomePage> {
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: const Text('Setel'),
+        backgroundColor: Colors.transparent,
         actions: <Widget>[
           IconButton(
             onPressed: () {
