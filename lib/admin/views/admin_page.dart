d05bd71d55e49d3338e60cf9e2e8204e5909fa61
Ezra Gunn <ezracodes@gmail.com>
2020-11-24 00:04:46 +0800

    Added fab and listview for loading and load success

--- lib/admin/views/admin_page.dart
+++ lib/admin/views/admin_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:setel_geofence/admin/blocs/stations/stations_bloc.dart';
+import 'package:setel_geofence/common_widgets/illustrated_message.dart';
+import 'package:setel_geofence/common_widgets/loader.dart';
+import 'package:setel_geofence/home/models/station/station.dart';
+import 'package:undraw/illustrations.dart';
 
 class AdminPage extends StatelessWidget {
   static const String routeName = '/admin';
@@ -12,8 +18,57 @@ class AdminPage extends StatelessWidget {
             snap: true,
             floating: true,
             title: Text('Admin'),
+          ),
+          SliverToBoxAdapter(
+            child: BlocBuilder<StationsBloc, StationsState>(
+              builder: (BuildContext context, StationsState state) {
+                if (state is StationsLoadSuccess) {
+                  final List<Station> stations = state.stations;
+
+                  return ListView.separated(
+                    shrinkWrap: true,
+                    physics: const ClampingScrollPhysics(),
+                    itemBuilder: (BuildContext context, int index) {
+                      final Station station = stations[index];
+
+                      return ListTile(
+                        title: Text(station.name),
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) {
+                      return const Divider();
+                    },
+                    itemCount: stations.length,
+                  );
+                } else if (state is StationsLoadInProgress) {
+                  return Loader(
+                    isShimmer: true,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      physics: const ClampingScrollPhysics(),
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          tileColor: Theme.of(context).dividerColor,
+                        );
+                      },
+                      itemCount: 6,
+                    ),
+                  );
+                }
+                return const IllustratedMessage(
+                  illustration: UnDrawIllustration.server_down,
+                  title: 'Could not load',
+                  subtitle: 'It seems like we are having difficulty in loading',
+                );
+              },
+            ),
           )
         ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.add),
+        label: const Text('Station'),
+        onPressed: () {},
       ),
     );
   }
