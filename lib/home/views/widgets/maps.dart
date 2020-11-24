7cf6e851a528c9bd8f6f4be80210de19ec656355
Ezra Gunn <ezracodes@gmail.com>
2020-11-24 18:40:31 +0800

    Added on tap circle when user in geofence

--- lib/home/views/widgets/maps.dart
+++ lib/home/views/widgets/maps.dart
@@ -2,14 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_geofence/geofence.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:setel_geofence/common_widgets/station/station_details.dart';
 import 'package:setel_geofence/home/blocs/current_location/current_location_bloc.dart';
+import 'package:setel_geofence/home/blocs/geofence/geofence_bloc.dart';
 import 'package:setel_geofence/home/cubits/map/map_cubit.dart';
 import 'package:setel_geofence/home/models/station/station.dart';
 import 'package:setel_geofence/resources/colours.dart';
 
-class Maps extends StatelessWidget {
+class Maps extends StatefulWidget {
   const Maps({@required this.stations});
   final List<Station> stations;
+
+  @override
+  _MapsState createState() => _MapsState();
+}
+
+class _MapsState extends State<Maps> {
+  void _showStationDetailsBottomSheet(
+      {@required BuildContext context, @required Station station}) {
+    showModalBottomSheet<dynamic>(
+      context: context,
+      isScrollControlled: true,
+      enableDrag: true,
+      builder: (BuildContext context) {
+        return DraggableScrollableSheet(
+          initialChildSize: 0.5,
+          expand: false,
+          minChildSize: 0.3,
+          maxChildSize: 0.9,
+          builder: (BuildContext context, ScrollController scrollController) {
+            return StationDetails(
+              station: station,
+              scrollController: scrollController,
+              isAdmin: false,
+            );
+          },
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +61,7 @@ class Maps extends StatelessWidget {
             target: LatLng(coordinate.latitude, coordinate.longitude),
             zoom: 18.0,
           ),
-          circles: stations.map((Station station) {
+          circles: widget.stations.map((Station station) {
             return Circle(
               circleId: CircleId(station.id),
               radius: double.parse(station.radius.toString()),
@@ -41,7 +72,16 @@ class Maps extends StatelessWidget {
               fillColor: setelBlue100.withOpacity(0.5),
               strokeWidth: 0,
               onTap: () {
-                print('Tapped on ${station.name}');
+                final GeofenceState geofenceState =
+                    context.read<GeofenceBloc>().state;
+                if (geofenceState is GeofenceEnterSuccess) {
+                  if (geofenceState.station.id == station.id) {
+                    _showStationDetailsBottomSheet(
+                      context: context,
+                      station: station,
+                    );
+                  }
+                }
               },
               consumeTapEvents: true,
             );
