00e27146ba33067404ca9cecb6ca60952fefe270
Ezra Gunn <ezracodes@gmail.com>
2020-11-24 16:49:36 +0800

    added get current location and animate to location

--- lib/home/views/home_page.dart
+++ lib/home/views/home_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_geofence/geofence.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:setel_geofence/admin/blocs/stations/stations_bloc.dart';
 import 'package:setel_geofence/admin/views/admin_page.dart';
 import 'package:setel_geofence/common_widgets/illustrated_message.dart';
 import 'package:setel_geofence/common_widgets/loader.dart';
+import 'package:setel_geofence/home/blocs/current_location/current_location_bloc.dart';
 import 'package:setel_geofence/home/blocs/geofence/geofence_bloc.dart';
 import 'package:setel_geofence/home/blocs/permission/permission_bloc.dart';
+import 'package:setel_geofence/home/cubits/map/map_cubit.dart';
 import 'package:setel_geofence/home/models/station/station.dart';
 import 'package:setel_geofence/home/views/widgets/maps.dart';
 import 'package:undraw/undraw.dart';
@@ -86,10 +90,27 @@ class _HomePageState extends State<HomePage> {
         floatingActionButton: BlocBuilder<PermissionBloc, PermissionState>(
           builder: (BuildContext context, PermissionState state) {
             if (state is PermissionRequestSuccess) {
-              return FloatingActionButton(
-                onPressed: () {},
-                child: const Icon(Icons.my_location),
-                foregroundColor: Colors.black,
+              return BlocBuilder<CurrentLocationBloc, CurrentLocationState>(
+                builder:
+                    (BuildContext context, CurrentLocationState locationState) {
+                  if (locationState is CurrentLocationLoadSuccess) {
+                    final Coordinate coordinate = locationState.coordinate;
+
+                    return FloatingActionButton(
+                      onPressed: () {
+                        context
+                            .read<CurrentLocationBloc>()
+                            .add(LoadCurrentLocation());
+                        context.read<MapCubit>().animateToLocation(
+                              LatLng(coordinate.latitude, coordinate.longitude),
+                            );
+                      },
+                      child: const Icon(Icons.my_location),
+                      foregroundColor: Colors.black,
+                    );
+                  }
+                  return const SizedBox.shrink();
+                },
               );
             }
             return const SizedBox.shrink();
