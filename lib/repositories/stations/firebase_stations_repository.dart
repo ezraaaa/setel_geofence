67d7a4d9d5bc338d80375e5c5d0d69950a82d415
Ezra Gunn <ezracodes@gmail.com>
2020-11-24 12:16:20 +0800

    Added add station

--- lib/repositories/stations/firebase_stations_repository.dart
+++ lib/repositories/stations/firebase_stations_repository.dart
@@ -3,12 +3,15 @@ import 'package:setel_geofence/home/models/station/station.dart';
 import 'package:setel_geofence/repositories/stations/stations_repository.dart';
 
 class FirebaseStationsRepository extends StationsRepository {
-  final CollectionReference _geofencesRef =
-      FirebaseFirestore.instance.collection('geofences');
+  final CollectionReference _stationsRef =
+      FirebaseFirestore.instance.collection('stations');
 
   @override
   Stream<List<Station>> getStations() {
-    return _geofencesRef.snapshots().map((QuerySnapshot snapshot) {
+    return _stationsRef
+        .orderBy('name')
+        .snapshots()
+        .map((QuerySnapshot snapshot) {
       return snapshot.docs.map((DocumentSnapshot data) {
         final Map<String, dynamic> dataWithID =
             Map<String, dynamic>.from(data.data()
@@ -20,5 +23,10 @@ class FirebaseStationsRepository extends StationsRepository {
         return Station.fromJson(dataWithID);
       }).toList();
     });
+  }
+
+  @override
+  Future<void> addStation(Map<String, dynamic> formData) {
+    return _stationsRef.add(formData);
   }
 }
--- lib/repositories/stations/stations_repository.dart
+++ lib/repositories/stations/stations_repository.dart
@@ -2,4 +2,6 @@ import 'package:setel_geofence/home/models/station/station.dart';
 
 abstract class StationsRepository {
   Stream<List<Station>> getStations();
+
+  Future<void> addStation(Map<String, dynamic> formData);
 }
