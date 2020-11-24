6f62adac53a504fa0ae1374bcdecacb2f1da0ed9
Ezra Gunn <ezracodes@gmail.com>
2020-11-24 23:29:35 +0800

    Added wifi bloc and ssid bloc

--- lib/app/app_state_provider.dart
+++ lib/app/app_state_provider.dart
@@ -8,6 +8,8 @@ import 'package:setel_geofence/app/app.dart';
 import 'package:setel_geofence/home/blocs/current_location/current_location_bloc.dart';
 import 'package:setel_geofence/home/blocs/geofence/geofence_bloc.dart';
 import 'package:setel_geofence/home/blocs/permission/permission_bloc.dart';
+import 'package:setel_geofence/home/blocs/ssid/ssid_bloc.dart';
+import 'package:setel_geofence/home/blocs/wifi/wifi_bloc.dart';
 import 'package:setel_geofence/home/cubits/map/map_cubit.dart';
 import 'package:setel_geofence/repositories/stations/firebase_stations_repository.dart';
 
@@ -40,6 +42,16 @@ class AppStateProvider extends StatelessWidget {
               return GeofenceBloc(
                 stationsBloc: BlocProvider.of<StationsBloc>(context),
               )..add(InitiateGeofence());
+            },
+          ),
+          BlocProvider<WifiBloc>(
+            create: (_) => WifiBloc(),
+          ),
+          BlocProvider<SsidBloc>(
+            create: (BuildContext context) {
+              return SsidBloc(
+                stationsBloc: BlocProvider.of<StationsBloc>(context),
+              );
             },
           ),
           BlocProvider<StationFormCubit>(
